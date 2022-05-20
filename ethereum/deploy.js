516df2d1471f7d4ef8f2a18c027cb71afeaed262
marceta99 <marcetaking@gmail.com>
2022-05-20 15:27:28 +0200

    initial

---
+++ ethereum/deploy.js
@@ -0,0 +1,27 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
+
+const provider = new HDWalletProvider(
+  'police similar bird home select first illness apart neglect valley humor hawk',
+  // remember to change this to your own phrase!
+  'https://rinkeby.infura.io/v3/8f3b5182a71d43e1bb85fb0b6095ef2a'
+  // remember to change this to your own endpoint!
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
